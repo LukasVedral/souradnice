cfb8543d9586666b574fe3ef77c974323bf6ccd2
lukas.vedral.023 <lukas.vedral.023@PC2023M-14>
2025-01-06 08:18:40 +0100

    l

---
+++ Coord.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace souradnice
+{
+    public class Coord
+    {
+        public Coord(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    ​
+            public int X { get; set; }
+        public int Y { get; set; }
+    }
+}
+
